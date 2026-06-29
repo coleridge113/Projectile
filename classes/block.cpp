c283f0901482c466ab3824b2b1178de09dc4afaa
Jose Luna <josemanuelmluna@gmail.com>
2026-06-29 14:59:16 +0800

    feat: added horizontal velocity

--- classes/block.cpp
+++ classes/block.cpp
@@ -1,5 +1,6 @@
 #include "block.h"
 #include "raylib.h"
+#include <cstdlib>
 
 Block::Block(float posX, float posY)
      :posX(posX), posY(posY), width(10), height(20), color(WHITE)
@@ -13,4 +14,5 @@ void Block::draw() const
 void Block::move(float velocity)
 {
     posY += velocity;
+    posX += std::abs(velocity / 8) * -1;
 }
